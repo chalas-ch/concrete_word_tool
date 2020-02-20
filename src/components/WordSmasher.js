2599dcc6cb4c31a061e395db68960d5d5825b465
chalas <charles@chalas.ch>
2020-02-20 11:42:52 +0100

    changed favicon, changed tool layout

--- src/components/WordSmasher.js
+++ src/components/WordSmasher.js
@@ -9,48 +9,52 @@ import ShuffleLetters from "./ShuffleLetters";
 import DeleteWords from "./DeleteWords";
 import InvisibleWords from "./InvisibleWords";
 
+import styles from '../scss/WordSmasher.module.scss';
+
 //import PropTypes from 'prop-types';
 
 class WordSmasher extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			input: ""
-		};
+        this.state = {
+            input: ""
+        };
 
-		this.handleChange = this.handleChange.bind(this);
-	}
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-	handleChange(event) {
-		this.setState({
-			input: event.target.value
-		});
-	}
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
 
-	render() {
-		return (
-			<div>
-				<br/>
-				<textarea rows="10" cols="50"
-                          value={this.state.input}
-                          onChange={this.handleChange}
-                          className="input_textarea"
-						  placeholder="type here"
-				/>
-				<br/>
-				<InvisibleWords input={this.state.input}/>
-				<DeleteWords input={this.state.input}/>
-				<ShuffleLetters input={this.state.input}/>
-				<RepeatLetters input={this.state.input}/>
-				<Reverse input={this.state.input}/>
-				<Pyramid input={this.state.input}/>
-				<ReversePyramid input={this.state.input}/>
-				<Apothegm input={this.state.input}/>
-                {/*<Datamuse input={this.state.input}/>*/}
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className={styles.content}>
+                <div className={styles.inputArea}>
+					<textarea rows="10" cols="50"
+							  value={this.state.input}
+							  onChange={this.handleChange}
+							  className="input_textarea"
+							  placeholder="type here"
+					/>
+                </div>
+                <div className={styles.outputArea}>
+                    <InvisibleWords input={this.state.input}/>
+                    <DeleteWords input={this.state.input}/>
+                    <ShuffleLetters input={this.state.input}/>
+                    <RepeatLetters input={this.state.input}/>
+                    <Reverse input={this.state.input}/>
+                    <Pyramid input={this.state.input}/>
+                    <ReversePyramid input={this.state.input}/>
+                    <Apothegm input={this.state.input}/>
+                    {/*<Datamuse input={this.state.input}/>*/}
+                </div>
+            </div>
+        );
+    }
 }
 
 //WordSmasher.propTypes = {};
